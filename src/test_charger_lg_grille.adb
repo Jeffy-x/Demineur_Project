33f16f3004c13aeb51ce76ddd5a3e7fe695a5820
Jeffy-x <jn.farge@yahoo.fr>
2023-12-30 12:38:31 +0100

    test_charger_lg_grille (first test)

---
+++ src/test_charger_lg_grille.adb
@@ -0,0 +1,21 @@
+with Ada.Text_IO, Pack_Demineur;
+use Ada.Text_IO, Pack_Demineur;
+
+procedure test_charger_lg_grille is
+    nb_lignes : T_Nb_Ligne;
+    nb_colonnes : T_Nb_Colonne;
+    titre : T_Chaine;
+begin
+    Put_Line ("Donnez le titre de la partie dont vous"
+    & " voulez charger la longueur : ");
+    Get_Line (titre.lettres, titre.longueur_chaine);
+    charger_lg_grille
+      (nb_lignes   => nb_lignes,
+       nb_colonnes => nb_colonnes,
+       titre       => titre);
+    Put ("La partie que vous avez charge a un nb de lignes : ");
+    Put (nb_lignes);
+    New_Line;
+    Put ("et un nb de colonnes : ");
+    Put (nb_colonnes);
+end test_charger_lg_grille;
